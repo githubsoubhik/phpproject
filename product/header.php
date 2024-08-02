9a2590901b10870a3b55002ee3f1a229471c9d4f
githubsoubhik <155788630+githubsoubhik@users.noreply.github.com>
2024-08-02 17:10:56 +0530

    Delete product directory

--- product/header.php
+++
@@ -1,9 +0,0 @@
-
-<div class: style="display: flex; width: 100%; height: auto; justify-content: space-evenly;align-items: center; font-size: 1.5rem; line-height: 2rem; font-weight: 800;">
-<a style="text-decoration: none; color: #26470f;" href="index.php">Add Product In Main Stock</a> 
-<a style="text-decoration: none; color: #26470f;" href="view_products.php">View Main Stock</a>  
-<a style="text-decoration: none; color: #26470f;" href="add_new_product.php">Add New Product for service</a>
-<a style="text-decoration: none; color: #26470f;" href="return_product_form.php">Return Product </a>
-<a style="text-decoration: none; color: #26470f;" href="return_product_details.php">Return Product List </a>
- 
-</div>
